5897b200afe728efd5b6a1bb736f4158772b00b5
clementPere <clement.pere.dev@gmail.com>
2022-01-28 10:25:21 +0100

    add type of function getArticles

--- src/Models/ArticleManager.php
+++ src/Models/ArticleManager.php
@@ -8,7 +8,7 @@ class ArticleManager extends Model
 {
 
 
-    public function getArticles()
+    public function getArticles(): array
     {
         $article = new Article();
         return $this->getAll('articles');
